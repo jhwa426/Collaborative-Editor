fa57f5ffc6151485c560ace5040a47f692655e15
Jeff Hwang <wodud6359@gmail.com>
2024-08-26 13:48:29 +1200

    Share-modal

--- collaborative-editor/components/ShareModal.tsx
+++ collaborative-editor/components/ShareModal.tsx
@@ -13,6 +13,12 @@ import { useSelf } from "@liveblocks/react/suspense";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
+import { updateDocumentAccess } from "@/lib/actions/room.actions";
+
 
 
 
@@ -26,9 +32,16 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: Share
     const [userType, setUserType] = useState<UserType>('viewer'); //"creator" | "editor" | "viewer";
 
     const shareDocumentHandler = async () => {
+        setLoading(true);
+
+        await updateDocumentAccess({
+            roomId,
+            email,
+            userType: userType as UserType,
+            updatedBy: user.info,
+        });
+
         setLoading(false);
-
-
     }
 
 
@@ -48,14 +61,48 @@ const ShareModal = ({ roomId, collaborators, creatorId, currentUserType }: Share
                     </p>
                 </Button>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent className="shad-dialog">
                 <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
-                    <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
-                    </DialogDescription>
+                    <DialogTitle>Manage who can view this project</DialogTitle>
+                    <DialogDescription>Select which users can view and edit this document</DialogDescription>
                 </DialogHeader>
+                <Label htmlFor="email" className="mt-6 text-blue-100">
+                    Email address
+                </Label>
+                <div className="flex items-center gap-3">
+                    <div className="flex flex-1 rounded-md bg-dark-400">
+                        <Input
+                            id="email"
+                            placeholder="Enter email address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="share-input"
+                        />
+                        <UserTypeSelector
+                            userType={userType}
+                            setUserType={setUserType}
+                        />
+                    </div>
+                    <Button type="submit" onClick={shareDocumentHandler} className="gradient-blue flex h-full gap-1 px-5" disabled={loading}>
+                        {loading ? 'Sending...' : 'Invite'}
+                    </Button>
+                </div>
+
+                <div className="my-2 space-y-2">
+                    <ul className="flex flex-col">
+                        {collaborators.map((collaborator) => (
+                            <Collaborator
+                                key={collaborator.id}
+                                roomId={roomId}
+                                creatorId={creatorId}
+                                email={collaborator.email}
+                                collaborator={collaborator}
+                                user={user.info}
+                            />
+                        ))}
+                    </ul>
+                </div>
+
             </DialogContent>
         </Dialog>
 
--- collaborative-editor/lib/actions/room.actions.tsx
+++ collaborative-editor/lib/actions/room.actions.tsx
@@ -3,7 +3,7 @@
 import { nanoid } from 'nanoid';
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
-import { parseStringify } from "../utils";
+import { getAccessType, parseStringify } from "../utils";
 
 
 // create Document
@@ -24,7 +24,7 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
         const room = await liveblocks.createRoom(roomId, {
             metadata,
             usersAccesses,
-            defaultAccesses: ['room:write'],
+            defaultAccesses: [],
         });
 
         revalidatePath('/');
@@ -43,13 +43,11 @@ export const getDocument = async ({ roomId, userId }: { roomId: string, userId: 
     try {
         const room = await liveblocks.getRoom(roomId);
 
-        // TODO: Update soon
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
 
-        // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
-
-        // if (!hasAccess) {
-        //     throw new Error('You do not have access to this document');
-        // }
+        if (!hasAccess) {
+            throw new Error('You do not have access to this document');
+        }
 
         return parseStringify(room);
 
@@ -89,6 +87,56 @@ export const getDocuments = async (email: string) => {
 
     } catch (error) {
         console.log(`Error happened while getting all rooms: ${error}`);
+    }
+}
+
+
+
+// Update Document Access
+export const updateDocumentAccess = async ({ roomId, email, userType, updatedBy }: ShareDocumentParams) => {
+    try {
+        const usersAccesses: RoomAccesses = {
+            [email]: getAccessType(userType) as AccessType,
+        }
+
+        const room = await liveblocks.updateRoom(roomId, { usersAccesses });
+
+        if (room) {
+            // TODO: Send a notification to the user
+        }
+
+        revalidatePath(`/documents/${roomId}`);
+
+        return parseStringify(room);
+
+    } catch (error) {
+        console.log(`Error happened while updating a room access: ${error}`);
+    }
+}
+
+
+
+// Remove Collaborator
+export const removeCollaborator = async ({ roomId, email }: { roomId: string, email: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+
+        if (room.metadata.email === email) {
+            throw new Error('You cannot remove yourself from the document');
+        }
+
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            usersAccesses: {
+                [email]: null
+            }
+        });
+
+        revalidatePath(`/documents/${roomId}`);
+
+        return parseStringify(updatedRoom);
+
+    } catch (error) {
+        console.log(`Error happened while removing a collaborator: ${error}`);
     }
 }
 
